ec4e7672210629319e92a0c1b388d511b24a6d2e
tan.le <leminhtanvatc@outlook.com>
2019-05-15 21:49:01 +0700

    Done StarGAN for FEI Face

---
+++ Src/StarGAN/Tools/CropImages/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace CropImages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var convertedDirectory = $"{args[0]}\\Converted";
+
+            if (!Directory.Exists(convertedDirectory))
+            {
+                Directory.CreateDirectory(convertedDirectory);
+            }
+
+            foreach(var file in Directory.EnumerateFiles(args[0], "*.jpg", SearchOption.TopDirectoryOnly))
+            {
+                using (var stream = File.OpenRead(file))
+                {
+                    var image = Image.FromStream(stream);
+                    var newImage = new Bitmap(image, new Size(256, 256));
+
+                    newImage.Save($"{convertedDirectory}\\{new FileInfo(file).Name}");
+                }
+            }
+        }
+    }
+}
